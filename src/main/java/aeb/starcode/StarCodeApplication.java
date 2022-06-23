ec14340bbe67eb56e9314af9993872d166f96116
Daniil Davydov <clairdess.github@gmail.com>
2022-06-23 23:45:31 +0900

    Initial commit

---
+++ src/main/java/aeb/starcode/StarCodeApplication.java
@@ -0,0 +1,13 @@
+package aeb.starcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aeb/starcode/StarCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package aeb.starcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
